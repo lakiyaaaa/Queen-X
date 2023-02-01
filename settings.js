924f02d599713cb929de9cc60119432973db0c02
lakiyaaaa <124119752+lakiyaaaa@users.noreply.github.com>
2023-02-01 12:41:22 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -22,7 +22,7 @@ global.socialm = "👸🏻ＱＵＥＥＮ-Ｘ ＢＯＴ👸🏻" //ur github or 
 global.location = "India, Mizoram, Aizawl" //ur location
 
 //bot bomdy 
-global.ownernomer = "94719225807" //ur number
+global.ownernomer = "94784441238" //ur number
 global.premium = ['94719225807'] //ur premium number
 global.botname = '©Qᴜᴇᴇɴ-x ᴡᴀᴛꜱᴀᴘᴘ ᴜꜱᴇʀ ʙᴏᴛ' //ur bot name
 global.linkz = "https://chat.whatsapp.com/Fd5MPEBn7NQ1aviL4V5fa7" //your theme url which will be displayed on whatsapp
